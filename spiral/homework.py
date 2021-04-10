dbfa77a72ab549d8ca105fd5b0345bb8eddac34e
vcu-stricklercb <78453263+vcu-stricklercb@users.noreply.github.com>
2021-04-09 21:13:11 -0400

    Update homework.py

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,8 @@
 def spiralize(number):
-    return_value = 1
-
-    return return_value
+    n=501
+    Spiralmatrix= None
+    Spiralmatrix_first_diagonal = sum(Spiralmatrix[i][i] for i in range(n))
+    Spiralmatrix_second_diagonal = sum(Spiralmatrix[i][n-i-1] for i in range(n)) - 1
+    print(str(Spiralmatrix_first_diagonal)+" "+str(Spiralmatrix_second_diagonal)) 
+    Value = Spiralmatrix_first_diagonal + Spiralmatrix_second_diagonal
+    print(Value)
